8da5b93e774df2766ef7b6e5ebf298d4ff0c0840
syamala <syamala.s4215@gmail.com>
2025-10-26 16:09:31 -0500

    files has been added

---
+++ emailssplit.py
@@ -0,0 +1,3 @@
+emails = ["ajay@gmail.com", "neha@yahoo.com", "ritu@outlook.com"]
+for e in emails:
+    print(e.split("@")[1])
---
+++ wordsreverse.py
@@ -0,0 +1,6 @@
+sentence="I love Python"
+s= sentence.split(" ")
+reversed_sentence = sentence[1::-1]
+#print("reversed:",s[2],s[1],s[0])
+print("Reversed sentence:",reversed_sentence)
+
